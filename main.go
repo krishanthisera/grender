20a58a2c0d15a119b79e36d4280343ad09d4e912
Krishan Thisera <krishan.thisera@yahoo.com>
2023-11-17 15:09:41 +1030

    include the flag to pass config file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/krishanthisera/grender/pilot"
@@ -8,7 +9,10 @@ import (
 
 func main() {
 	fmt.Println("Grender is starting...")
-	grenderConfig, err := pilot.GenerateConfig("grender.yaml")
+
+	cfg := flag.String("config", "grender.yaml", "Path to grender config file")
+
+	grenderConfig, err := pilot.GenerateConfig(*cfg)
 	fmt.Print(grenderConfig)
 	if err != nil {
 		panic(err)
